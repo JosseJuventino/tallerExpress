2c6101bf54858f3972daf6a74f417b338b4e15df
JosseJuventino <00048322@uca.edu.sv>
2023-11-14 14:32:59 -0600

    :stars: middlewares and controllers

---
+++ src/controllers/student.controller.js
@@ -0,0 +1,31 @@
+const httpError = require("http-errors");
+
+const Student = require("../model/Student.model");
+
+const getAllStudents = async (req, res, next) => {
+  try {
+    const students = await Student.find();
+    if (!students) throw httpError(404, "Student not found");
+    res.status(200).json({ data: movies });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const createStudent = async (req, res, next) => {
+  try {
+    const { body } = req;
+    const newStudent = new Student(body);
+    const savedStudent = await newStudent.save();
+
+    if (!savedStudent) throw httpError(500, "Student not created");
+    res.status(201).json({ message: "Student created", data: savedStudent });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = {
+  getAllStudents,
+  createStudent,
+};
