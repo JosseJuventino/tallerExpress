db77b26453feb3cb3664e4c53b0d400e370ab899
Merino <0036822@uca.edu.sv>
2023-11-14 14:50:41 -0600

    add routers

--- src/routes/Student.router.js
+++ src/routes/Student.router.js
@@ -10,5 +10,11 @@ const { route } = require('express/lib/application');
 router.get('/', getAllStudents);
 router.post('/', createStudent);
 
+const { createStudentValidator, idInParams } = require('../validators/student.validator');
+const {runValidation} = require('../middlewares/validator.middleware');
+
+router.post('/', createStudentValidator, runValidation, createStudent);
+router.get('/:id', idInParams, runValidation, getStudentById);
+
 
 module.exports = router;
